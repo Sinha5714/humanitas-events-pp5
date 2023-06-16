6812e777b75da0985912fa36f289b7efa406290b
Sinha5714 <>
2023-06-16 17:51:38 +0000

    Add refresh interceptor so that user can be logged in for minimum of 24 hours

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,5 +1,6 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import { axiosRes } from "../api/axiosDefaults";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -12,7 +13,7 @@ export const CurrentUserProvider = ({children}) => {
 
     const handleMount = async() =>{
         try{
-            const {data} = await axios.get('dj-rest-auth/user/')
+            const {data} = await axiosRes.get('dj-rest-auth/user/')
             setCurrentUser(data)
         }catch(err){
             console.log(err)
@@ -22,6 +23,30 @@ export const CurrentUserProvider = ({children}) => {
     useEffect(()=>{
         handleMount()
     }, []);
+
+    useMemo(() => {
+        axiosRes.interceptors.response.use(
+            (response) => response,
+            async (err) => {
+                if(err.response?.status === 401){
+                    try{
+                        await axios.post("/dj-rest-auth/token/refresh/");
+                    }catch(err){
+                        setCurrentUser((prevCurrentUser) =>{
+                            if(prevCurrentUser){
+                                history.push("/signin");
+                            }
+                            return null;
+                        });
+                    }
+                    return axios(err.config);
+                }
+                return Promise.reject(err);
+            }
+        )
+    }
+
+    )
 
     return(
         <CurrentUserContext.Provider value={currentUser}>
