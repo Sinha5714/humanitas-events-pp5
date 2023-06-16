d0266ad1c3e1c9c093ffb89d6382f4b7f7fee9a8
Sinha5714 <>
2023-06-16 17:03:22 +0000

    Add handleMount function which get data of user and display to user

--- src/App.js
+++ src/App.js
@@ -5,21 +5,38 @@ import { Container } from "react-bootstrap";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className={styles.App}>
-        <NavBar/>
-        <Container className={styles.Main}>
-            <Switch>
-                <Route exact path="/" render={() => <h1>Home page</h1>} />
-                <Route exact path="/signin" render={() => <SignInForm />} />
-                <Route exact path="/signup" render={() => <SignUpForm />} />
-                <Route render={() => <p>Page not found!</p>} />
-            </Switch>
-        </Container>
-    </div>
-  );
+    const[currentUser, setCurrentUser] = useState(null)
+
+    const handleMount = async() =>{
+        try{
+            const {data} = await axios.get('dj-rest-auth/user/')
+            setCurrentUser(data)
+        }catch(err){
+            console.log(err)
+        }
+    };
+
+    useEffect(()=>{
+        handleMount()
+    }, [])
+
+    return (
+        <div className={styles.App}>
+            <NavBar/>
+            <Container className={styles.Main}>
+                <Switch>
+                    <Route exact path="/" render={() => <h1>Home page</h1>} />
+                    <Route exact path="/signin" render={() => <SignInForm />} />
+                    <Route exact path="/signup" render={() => <SignUpForm />} />
+                    <Route render={() => <p>Page not found!</p>} />
+                </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
