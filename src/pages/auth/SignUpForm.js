442faf8c3979b2e250bc057a76b3b73a19955a43
Sinha5714 <>
2023-06-16 16:02:16 +0000

    Add basic code for  signupform using react bootstrap

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import {Col, Row, Container, Button, Form } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
-function SignUpForm() {
+const SignUpForm = () => {
   return (
-    <div>SignUpForm</div>
-  )
-}
+    <Row>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className="p-4">
+          <h1>Sign Up</h1>
+          <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Username" />
+                </Form.Group>
 
-export default SignUpForm
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword2">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+        <Container className="mt-3">
+          <Link to="/signin">
+            Already have an account? <span>Sign in</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className="my-auto d-none d-md-block p-2">
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
