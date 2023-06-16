71bb7569c88a961171f7013724a2fc007ce9be25
Sinha5714 <>
2023-06-16 13:15:39 +0000

    Axios: Install axios and connect backend api to the site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styles from "./App.module.css";
 import NavBar from './components/NavBar';
 import {Route, Switch} from 'react-router-dom';
 import { Container } from "react-bootstrap";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
             <Switch>
                 <Route exact path="/" render={() => <h1>Home page</h1>} />
                 <Route exact path="/signin" render={() => <h1>Login</h1>} />
-                <Route exact path="/signup" render={() => <h1>Register</h1>} />
+                <Route exact path="/signup" render={() => <SignUpForm />} />
                 <Route render={() => <p>Page not found!</p>} />
             </Switch>
         </Container>
