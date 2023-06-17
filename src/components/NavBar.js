3f993a695dc8f64863958c778ac079768f7259ad
Sinha5714 <>
2023-06-17 08:57:33 +0000

    Create a hook so that hamburger menu can be close by clicking on any navlinks or any where in page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import styles from '../styles/NavBar.module.css';
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
@@ -6,11 +6,14 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import axios from "axios";
 import Avatar from "./Avatar";
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
-    const setCurrentUser = useSetCurrentUser()
+    const setCurrentUser = useSetCurrentUser();
+
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
     const handleLogOut = async () =>{
         try {
@@ -59,7 +62,7 @@ const NavBar = () => {
     )
        
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
         <Container>
             <NavLink to="/">
                 <Navbar.Brand>
@@ -70,7 +73,10 @@ const NavBar = () => {
                 </Navbar.Brand>
             </NavLink>
             {currentUser && addEventIcon}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle
+            ref={ref}
+            onClick={()=> setExpanded(!expanded)}
+            aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
                 <NavLink className={styles.NavLink}
