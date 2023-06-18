e2efdefd5dcc5052d445ab1479f189ca6075254c
Sinha5714 <>
2023-06-18 17:23:16 +0000

    Add functionality to fetch an event using id

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq } from '../../api/axiosDefaults';
 import appStyles from "../../App.module.css";
 
 
 function EventPage() {
+
+    const {id} = useParams();
+    const [event, setEvent] = useState({results: []});
+
+    useEffect(()=>{
+        const handleMount = async () => {
+            try {
+                const [ {data : event}] = await Promise.all(
+                    [axiosReq.get(`/events/${id}`)]
+                )
+                setEvent({results : [event]});
+            } catch (err) {
+                console.log(err)
+            }
+        };
+        handleMount(); 
+    }, [id]);
   return (
     <>
         <Row className="h-100">
