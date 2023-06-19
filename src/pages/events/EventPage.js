1fc4f30704c186721cab54ccccadbdffa3b81f5e
Sinha5714 <>
2023-06-19 14:39:25 +0000

    Add commentCreateForm and added it to eventPage.js

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -3,13 +3,18 @@ import { Col, Container, Row } from 'react-bootstrap'
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { axiosReq } from '../../api/axiosDefaults';
 import appStyles from "../../App.module.css";
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import CommentCreateForm from '../comments/CommentCreateForm';
 import Event from './Event';
 
 
 function EventPage() {
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({results : []});
 
     const {id} = useParams();
-    const [event, setEvents] = useState({results: []});
+    const [event, setEvent] = useState({results: []});
 
     useEffect(()=>{
         const handleMount = async () => {
@@ -17,7 +22,7 @@ function EventPage() {
                 const [ {data : event}] = await Promise.all(
                     [axiosReq.get(`/events/${id}`)]
                 )
-                setEvents({results : [event]});
+                setEvent({results : [event]});
             } catch (err) {
                 console.log(err)
             }
@@ -29,8 +34,20 @@ function EventPage() {
         <Row className="h-100">
             <Col className='py-2 p-0 p-lg-2' lg={8}>
                 <p>Popular profiles for mobile</p>
-                <Event {...event.results[0]} setEvents={setEvents} eventPage />
-                <Container className={appStyles.Content}>Comments</Container>
+                <Event {...event.results[0]} setEvent={setEvent} eventPage />
+                <Container className={appStyles.Content}>
+                    {currentUser? (
+                        <CommentCreateForm
+                        profile_id={currentUser.profile_id}
+                        profileImage={profile_image}
+                        event={id}
+                        setEvent={setEvent}
+                        setComments={setComments} 
+                    />
+                    ) : comments.results.length? (
+                        "Comments"
+                    ) : null}
+                </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
                 Popular profiles for desktop
