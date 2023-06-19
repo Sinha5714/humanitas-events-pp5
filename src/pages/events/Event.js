1d023f63e58b6ce7bf745a6270403a384afdf108
Sinha5714 <>
2023-06-19 08:45:27 +0000

    Add overlay message for user to show interest or send join request if not logged in or owner of teh veent

--- src/pages/events/Event.js
+++ src/pages/events/Event.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Media } from 'react-bootstrap';
+import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Avatar from '../../components/Avatar';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
@@ -19,6 +19,9 @@ const Event = (props) => {
         title,
         content,
         event_start_date,
+        event_end_date,
+        category,
+        sub_category,
         image,
         updated_on,
         eventPage,
@@ -45,8 +48,56 @@ const Event = (props) => {
             <Card.Img src={image} alt={title} />
         </Link>
         <Card.Body>
-            {title && event_start_date && <Card.Title className='text-center'>{title} on {event_start_date}</Card.Title>}
+            {title && <Card.Title className='text-center'>{title}</Card.Title>}
+            {category && sub_category && <Card.Text className='text-center'>{category} - {sub_category}</Card.Text>}
+            {event_start_date && event_end_date && <Card.Text className='text-center'>{event_start_date} - {event_end_date}</Card.Text>}
             {content && <Card.Text>{content}</Card.Text>}
+            <div>
+                {is_owner?(
+                    <OverlayTrigger placement='top' overlay={<Tooltip>You can't show interest to your own event</Tooltip>}>
+                        <i className="far fa-star" />
+                    </OverlayTrigger>
+                ): interested_id?(
+                    <span onClick={() => {}}>
+                        <i className="fas fa-star" />
+                    </span>
+                ): currentUser? (
+                    <span onClick={()=>{}}>
+                        <i className='far fa-star' />
+                    </span>
+                ) : (
+                    <OverlayTrigger placement='top' overlay={<Tooltip>Log in to show your interest!</Tooltip>}>
+                        <i className="far fa-star" />
+                    </OverlayTrigger>
+                )}
+                <span className='mr-2'>{interested_count}</span>
+
+                {is_owner?(
+                    <OverlayTrigger placement='top' overlay={<Tooltip>You can't send join request to your own event</Tooltip>}>
+                        <i className="far fa-calendar-check" />
+                    </OverlayTrigger>
+                ): join_id?(
+                    <span onClick={() => {}}>
+                        <i className="fas fa-calendar-check" />
+                    </span>
+                ): currentUser? (
+                    <span onClick={()=>{}}>
+                        <i className='far fa-calendar-check' />
+                    </span>
+                ) : (
+                    <OverlayTrigger placement='top' overlay={<Tooltip>Log in to send a join request!</Tooltip>}>
+                        <i className="far fa-calendar-check" />
+                    </OverlayTrigger>
+                )}
+                <span className='mr-2'>{join_request}</span>
+                    
+                <Link to={`/events/${id}`}>
+                    <i className='far fa-comments'></i>
+                </Link>
+                {comments_count}
+
+            </div>
+
         </Card.Body>
     </Card>
   )
