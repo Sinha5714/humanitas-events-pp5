2d19266b68baae67e9496be8f98556fec1f5fe01
Sinha5714 <>
2023-06-21 09:45:22 +0000

    Add functionality for user to view profiles

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import appStyles from '../../App.module.css';
+import { useProfileData } from '../../contexts/ProfileDataContext';
 
 const PopularProfiles = () => {
+
+    const {popularProfiles} = useProfileData();
   return (
     <Container className={appStyles.Content}>
-        <p>Most followed profiles</p>
+        <h4 className='text-center'>Our Top Profiles</h4>
+        {popularProfiles.results.map(profile => (
+            <p key={profile.id}>{profile.user}</p>
+        ))}
+
     </Container>
    
   )
