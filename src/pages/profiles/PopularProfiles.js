8c7282528b51a2b231aee6a2ed25c9905da379bf
Sinha5714 <>
2023-06-21 09:55:20 +0000

    Add functionality to slice the popular profile so that user can only see top 6

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import appStyles from '../../App.module.css';
+import Asset from '../../components/Asset';
 import { useProfileData } from '../../contexts/ProfileDataContext';
 
 const PopularProfiles = () => {
 
     const {popularProfiles} = useProfileData();
   return (
-    <Container className={appStyles.Content}>
-        <h4 className='text-center'>Our Top Profiles</h4>
-        {popularProfiles.results.map(profile => (
-            <p key={profile.id}>{profile.user}</p>
-        ))}
-
+    <Container className={`${appStyles.Content} mb-3`}>
+        {popularProfiles.results.length ? (
+            <>
+                <h4 className='text-center'>Our Top Organisers</h4>
+                {popularProfiles.results.slice(0,6).map((profile) => (
+                    <Container key={profile.id} className='d-inline justify-content-center align-items-center'>
+                        {profile.user}
+                    </Container>                   
+                ))}
+            </>    
+        ) : (
+            <Asset spinner />
+        )}
+        
     </Container>
+
    
   )
 }
