5a1e8869e0009ba78600c937b605c1692f8e61f3
Sinha5714 <>
2023-06-21 16:17:20 +0000

    Update useEffect function to fetch data from api for profiles

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,69 +1,84 @@
 import React, { useEffect, useState } from "react";
-
-
-
 import Asset from "../../components/Asset";
-
 import styles from "../../styles/ProfilePage.module.css";
 import appStyles from "../../App.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Col, Container, Row } from "react-bootstrap";
 import PopularProfiles from "./PopularProfiles";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 function ProfilePage() {
-  const [hasLoaded, setHasLoaded] = useState(false);
-  const currentUser = useCurrentUser();
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const currentUser = useCurrentUser();
+    const {id} = useParams();
+    const setProfileData = useSetProfileData();
 
-  useEffect(() => {
-      setHasLoaded(true);
-  }, [])
+    useEffect(() => {
+        const fetchData = async () =>{
+            try {
+                const [{data : pageProfile}] = await Promise.all([
+                    axiosReq.get(`/profiles/${id}`)
+                ])
+                setProfileData(prevState => ({
+                    ...prevState,
+                    pageProfile: {results: [pageProfile]}
+                }))
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
+        }   
+        fetchData()
+    }, [id, setProfileData])
 
-  const mainProfile = (
-    <>
-      <Row noGutters className="px-3 text-center">
-        <Col lg={3} className="text-lg-left">
-          <p>Image</p>
+    const mainProfile = (
+        <>
+        <Row noGutters className="px-3 text-center">
+            <Col lg={3} className="text-lg-left">
+            <p>Image</p>
+            </Col>
+            <Col lg={6}>
+            <h3 className="m-2">Profile username</h3>
+            <p>Profile stats</p>
+            </Col>
+            <Col lg={3} className="text-lg-right">
+            <p>Follow button</p>
+            </Col>
+            <Col className="p-3">Profile content</Col>
+        </Row>
+        </>
+    );
+
+    const mainProfileEvents = (
+        <>
+        <hr />
+        <p className="text-center">Profile user's events</p>
+        <hr />
+        </>
+    );
+
+    return (
+        <Row>
+        <Col className="py-2 p-0 p-lg-2" lg={8}>
+            <PopularProfiles />
+            <Container className={appStyles.Content}>
+            {hasLoaded ? (
+                <>
+                {mainProfile}
+                {mainProfileEvents}
+                </>
+            ) : (
+                <Asset spinner />
+            )}
+            </Container>
         </Col>
-        <Col lg={6}>
-          <h3 className="m-2">Profile username</h3>
-          <p>Profile stats</p>
+        <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+            
         </Col>
-        <Col lg={3} className="text-lg-right">
-        <p>Follow button</p>
-        </Col>
-        <Col className="p-3">Profile content</Col>
-      </Row>
-    </>
-  );
-
-  const mainProfileEvents = (
-    <>
-      <hr />
-      <p className="text-center">Profile user's events</p>
-      <hr />
-    </>
-  );
-
-  return (
-    <Row>
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <PopularProfiles />
-        <Container className={appStyles.Content}>
-          {hasLoaded ? (
-            <>
-              {mainProfile}
-              {mainProfileEvents}
-            </>
-          ) : (
-            <Asset spinner />
-          )}
-        </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        
-      </Col>
-    </Row>
-  );
+        </Row>
+    );
 }
 
-export default ProfilePage;
+    export default ProfilePage;
