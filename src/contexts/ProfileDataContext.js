85ddba176e22734d4aa98a6211a0aa1863a8ca8c
Sinha5714 <>
2023-06-21 09:18:26 +0000

    ProfileDataContext: Add context for profile data for use in application from any page

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,7 @@
+import { createContext, useContext } from "react";
+
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
