856ea1260153d65e51db15d7fc508af1beb21ff6
Sinha5714 <>
2023-06-21 16:08:47 +0000

    Add basic structure to profile page using boiler plate from Moments walkthrough

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+
+
+import Asset from "../../components/Asset";
+
+import styles from "../../styles/ProfilePage.module.css";
+import appStyles from "../../App.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Col, Container, Row } from "react-bootstrap";
+import PopularProfiles from "./PopularProfiles";
 
 function ProfilePage() {
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+      setHasLoaded(true);
+  }, [])
+
+  const mainProfile = (
+    <>
+      <Row noGutters className="px-3 text-center">
+        <Col lg={3} className="text-lg-left">
+          <p>Image</p>
+        </Col>
+        <Col lg={6}>
+          <h3 className="m-2">Profile username</h3>
+          <p>Profile stats</p>
+        </Col>
+        <Col lg={3} className="text-lg-right">
+        <p>Follow button</p>
+        </Col>
+        <Col className="p-3">Profile content</Col>
+      </Row>
+    </>
+  );
+
+  const mainProfileEvents = (
+    <>
+      <hr />
+      <p className="text-center">Profile user's events</p>
+      <hr />
+    </>
+  );
+
   return (
-    <div>ProfilePage</div>
-  )
+    <Row>
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles />
+        <Container className={appStyles.Content}>
+          {hasLoaded ? (
+            <>
+              {mainProfile}
+              {mainProfileEvents}
+            </>
+          ) : (
+            <Asset spinner />
+          )}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        
+      </Col>
+    </Row>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
