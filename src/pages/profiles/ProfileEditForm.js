786baa99547c94f49fe58dac25f25e4108099dd7
Sinha5714 <>
2023-06-22 09:30:05 +0000

    Add data which can be edited in profile edit form

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useCurrentUser, useSetCurrentUser } from '../../contexts/CurrentUserContext'
 
 const ProfileEditForm = () => {
+    const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+    const {id} = useParams();
+    const history = useHistory();
+    const imageFile = useRef();
+
+    const [profileData, setProfileData] = useState({
+        name: "",
+        about_me: "",
+        profile_pic: "",
+        instagram_link: "",
+        facebook_link: "",
+        phone_number: "",
+        email: "",
+
+    });
+
+    const {
+        name,
+        about_me,
+        profile_pic,
+        instagram_link,
+        facebook_link,
+        phone_number,
+        email
+    } = profileData;
+
   return (
     <div>ProfileEditForm</div>
   )
