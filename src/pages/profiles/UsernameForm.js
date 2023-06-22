0b8294b66631d829947abb61fb1f962dbeebbae4
Sinha5714 <>
2023-06-22 11:58:36 +0000

    Add handle submit function to submit username form and also show error to user

--- src/pages/profiles/UsernameForm.js
+++ src/pages/profiles/UsernameForm.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Row, Form, Container, Button } from 'react-bootstrap';
+import { Col, Row, Form, Container, Button, Alert } from 'react-bootstrap';
 import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { useCurrentUser, useSetCurrentUser } from '../../contexts/CurrentUserContext';
 import appStyles from "../../App.module.css";
+import { axiosRes } from '../../api/axiosDefaults';
 
 const UsernameForm = () => {
     const [username, setUsername] = useState("");
@@ -21,11 +22,28 @@ const UsernameForm = () => {
         }
     }, [currentUser, history, id])
 
+    const handleSubmit = async(event) =>{
+        event.preventDefault();
+        try {
+            await axiosRes.put("/dj-rest-auth/user/", {
+                username,
+            });
+            setCurrentUser((prevUser) => ({
+                ...prevUser,
+                username,
+            }));
+            history.goBack();
+        } catch (err) {
+            console.log(err);
+            setErrors(err.response?.data);
+        }
+    }
+
   return (
     <Row>
         <Col className="py-2 mx-auto text-center" md={6}>
             <Container className={appStyles.Content}>
-                <Form onSubmit={()=>{}} className="my-2">
+                <Form onSubmit={handleSubmit} className="my-2">
                     <Form.Group>
                         <Form.Label>Change username</Form.Label>
                         <Form.Control
@@ -35,6 +53,11 @@ const UsernameForm = () => {
                         onChange={(event) => setUsername(event.target.value)}
                         />
                     </Form.Group>
+                    {errors?.username?.map((message, idx) => (
+                        <Alert key={idx} variant="warning">
+                            {message}
+                        </Alert>
+                    ))}
                     <Button onClick={() => history.goBack()}>Cancel</Button>
                     <Button type="submit">Save</Button>
                 </Form>
