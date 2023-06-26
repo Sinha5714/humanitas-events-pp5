4c4ccf30754c5d79934de472a272d26a7c09e7f2
Sinha5714 <>
2023-06-26 09:09:10 +0000

    Add currentUser in useEffect depedency array in EventsPage.js

--- src/pages/events/EventsPage.js
+++ src/pages/events/EventsPage.js
@@ -14,16 +14,16 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import { fetchMoreData } from '../../utils/utils';
 import PopularProfiles from '../profiles/PopularProfiles';
 import UpComingEvents from './UpComingEvents';
-
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 function EventsPage({message, filter=""}) {
     const [events, setEvents] = useState({results: []});
     const [hasLoaded, setHasLoaded] = useState(false);
     const {pathname} = useLocation();
-
+    const currentUser = useCurrentUser();
     const [query, setQuery] = useState("");
 
-    useEffect(()=>{
+    useEffect(()=> {
         const fetchEvents = async () => {
             try {
                 const {data} = await axiosReq.get(`/events/?${filter}search=${query}`)
@@ -35,57 +35,58 @@ function EventsPage({message, filter=""}) {
         }
         setHasLoaded(false)
         fetchEvents()
-    }, [filter,query, pathname])
-  return (
-    <Row className='h-100'>
-        <Col className='py-2 p-0 p-lg-2' lg={8}>
-            <PopularProfiles />
+    }, [filter, query, pathname, currentUser])
 
-            <UpComingEvents mobile />
+    return (
+        <Row className='h-100'>
+            <Col className='py-2 p-0 p-lg-2' lg={8}>
+                <PopularProfiles />
 
-            <Container>
-                <i className={`fas fa-search ${styles.SearchIcon}`} />
-                <Form
-                className={styles.SearchBar}
-                onSubmit={(event) => event.preventDefault()}>
-                    <Form.Control
-                    value={query}
-                    onChange={(event) => setQuery(event.target.value)} 
-                    type='text'
-                    className='mr-sm-2'
-                    placeholder='Search Events by title, username, date or category'
-                    />   
-                </Form>
-            </Container>
-            {hasLoaded? (
-                <>
-                    {events.results.length?(
-                        <InfiniteScroll
-                            children={events.results.map((event) => (
-                                <Event key={event.id} {...event} setEvents={setEvents} /> 
-                            ))}
-                            dataLength={events.results.length}
-                            loader={<Asset spinner />}
-                            hasMore={!!events.next}
-                            next={() => fetchMoreData(events, setEvents)} 
-                        />
-                    ): (
-                        <Container className={appStyles.Content}>
-                            <Asset src={NoResults} message={message} />
-                        </Container>
-                    )}
-                </>
-            ) : (
-                <Container className={appStyles.Content} >
-                    <Asset spinner />
+                <UpComingEvents mobile />
+
+                <Container>
+                    <i className={`fas fa-search ${styles.SearchIcon}`} />
+                    <Form
+                    className={styles.SearchBar}
+                    onSubmit={(event) => event.preventDefault()}>
+                        <Form.Control
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)} 
+                        type='text'
+                        className='mr-sm-2'
+                        placeholder='Search Events by title, username, date or category'
+                        />   
+                    </Form>
                 </Container>
-            )}        
-        </Col>
-        <Col md={4} className='d-none d-lg-block p-0 p-lg-2'>
-            <UpComingEvents />
-        </Col>
-    </Row>
-  )
+                {hasLoaded? (
+                    <>
+                        {events.results.length?(
+                            <InfiniteScroll
+                                children={events.results.map((event) => (
+                                    <Event key={event.id} {...event} setEvents={setEvents} /> 
+                                ))}
+                                dataLength={events.results.length}
+                                loader={<Asset spinner />}
+                                hasMore={!!events.next}
+                                next={() => fetchMoreData(events, setEvents)} 
+                            />
+                        ): (
+                            <Container className={appStyles.Content}>
+                                <Asset src={NoResults} message={message} />
+                            </Container>
+                        )}
+                    </>
+                ) : (
+                    <Container className={appStyles.Content} >
+                        <Asset spinner />
+                    </Container>
+                )}        
+            </Col>
+            <Col md={4} className='d-none d-lg-block p-0 p-lg-2'>
+                <UpComingEvents />
+            </Col>
+        </Row>
+    )
 }
 
 export default EventsPage
